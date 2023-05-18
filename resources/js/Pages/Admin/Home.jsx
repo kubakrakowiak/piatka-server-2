451ca542b7877c71f20780c7ce34a9e300ba3f52
kubakrakowiak <jakubkrakowiak00@gmail.com>
2023-05-19 00:51:01 +0200

    made basic panel

--- resources/js/Pages/Admin/Home.jsx
+++ resources/js/Pages/Admin/Home.jsx
@@ -1,21 +1,16 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import EventsForm from "@/Components/Admin/EventsForm";
 
 export default function Edit({ auth}) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Admin Control Panel</h2>}
         >
-            <Head title="Profile" />
+            <Head title="Admin" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        TEST
-                    </div>
-                </div>
-            </div>
+            <EventsForm/>
         </AuthenticatedLayout>
     );
 }
