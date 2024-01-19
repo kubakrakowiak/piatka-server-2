210a546bb4144d58904b70bfa3e6f092fc8151af
sewru <sewruczekk@icloud.com>
2024-01-19 20:43:48 +0100

    Test: UpdateProfileInformationForm.jsx

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -22,7 +22,8 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
     return (
         <section className={className}>
             <header>
-                <h2 className="text-lg font-medium text-gray-900">Profile Information</h2>
+                <h2 className="text-lg font-medium text-gray-900">Profile info (by seewruuk)</h2>
+                {/*<h2 className="text-lg font-medium text-gray-900">Profile Information</h2>*/}
 
                 <p className="mt-1 text-sm text-gray-600">
                     Update your account's profile information and email address.
