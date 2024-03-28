c4fe6a653f7a5149c76880d631c0ca837180a155
kubakrakowiak <jakubkrakowiak00@gmail.com>
2024-03-28 19:19:03 +0100

    modals added

--- resources/js/Pages/Admin/UserPermissions.jsx
+++ resources/js/Pages/Admin/UserPermissions.jsx
@@ -2,13 +2,17 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head} from '@inertiajs/react';
 import Header from "@/Components/Header/Header";
 import List from "@/Components/Events/List";
+import {useState} from "react";
 
 
 export default function Page({auth, user, systemRoles, companies}) {
 
-    console.log(user)
-    console.log(systemRoles)
-    console.log(companies)
+    const [isSystemPermissionModalOpen, setIsSystemPermissionModalOpen] = useState(false);
+    const [isCompanyPermissionModalOpen, setIsCompanyPermissionModalOpen] = useState(false);
+
+    const toggleSystemPermissionModal = () => setIsSystemPermissionModalOpen(!isSystemPermissionModalOpen);
+    const toggleCompanyPermissionModal = () => setIsCompanyPermissionModalOpen(!isCompanyPermissionModalOpen);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -22,10 +26,12 @@ export default function Page({auth, user, systemRoles, companies}) {
                 <div className="flex-1 p-4">
                     <div className="flex items-center justify-between">
                         <p>Uprawnienia Systemowe</p>
-                        <a href={`/users/add`}
-                           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <button
+                            onClick={toggleSystemPermissionModal}
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
                             Dodaj
-                        </a>
+                        </button>
                     </div>
                     <ul>
                         {systemRoles.map((role, index) => (
@@ -39,10 +45,12 @@ export default function Page({auth, user, systemRoles, companies}) {
                 <div className="flex-1 p-4">
                     <div className="flex items-center justify-between">
                         <p>Uprawnienia Organizatorów</p>
-                        <a href={`/users/add`}
-                           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <button
+                            onClick={toggleCompanyPermissionModal}
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
                             Dodaj
-                        </a>
+                        </button>
                         <ul>
                             {companies.map((company, index) => (
                                 <li key={index}>{company.name} {company.pivot.role_name}</li>
@@ -52,6 +60,53 @@ export default function Page({auth, user, systemRoles, companies}) {
                 </div>
             </div>
 
+            {isSystemPermissionModalOpen && (
+                <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-40">
+                    <div className="flex items-center justify-center min-h-screen">
+                        <div className="bg-white p-5 rounded-lg shadow-xl">
+                            <div className="mb-4">
+                                <h2 className="text-xl font-bold">Dodaj Uprawnienie Systemowe</h2>
+                            </div>
+                            <button
+                                onClick={toggleSystemPermissionModal}
+                                className="mt-4 bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded"
+                            >
+                                Zapisz
+                            </button>
+                            <button
+                                onClick={toggleSystemPermissionModal}
+                                className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                            >
+                                Zamknij
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {isCompanyPermissionModalOpen && (
+                <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-40">
+                    <div className="flex items-center justify-center min-h-screen">
+                        <div className="bg-white p-5 rounded-lg shadow-xl">
+                            <div className="mb-4">
+                                <h2 className="text-xl font-bold">Dodaj Uprawnienie Organizatorow</h2>
+                            </div>
+                            <button
+                                onClick={toggleCompanyPermissionModal}
+                                className="mt-4 bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded"
+                            >
+                                Zapisz
+                            </button>
+                            <button
+                                onClick={toggleCompanyPermissionModal}
+                                className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                            >
+                                Zamknij
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
 
         </AuthenticatedLayout>
     );
